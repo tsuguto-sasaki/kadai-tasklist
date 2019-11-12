515c31644658b9cf208ea942408c89ad3cea5c18
tsuguto.sasaki <sasaki.tsuguto0613@gmail.com>
2019-11-12 10:35:24 +0000

    カラム追加しました！

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_085437) do
+ActiveRecord::Schema.define(version: 2019_11_12_094510) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
