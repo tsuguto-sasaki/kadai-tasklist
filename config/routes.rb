eabaff8ff726b1e5ff1e9b13ef9a780f3b3be111
tsuguto.sasaki <sasaki.tsuguto0613@gmail.com>
2019-11-19 06:56:09 +0000

    ユーザー登録ページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'toppages', to: 'toppages#index'
   root to: 'tasks#index'
  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   
   resources :tasks
 end
